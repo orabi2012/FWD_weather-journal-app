cc808cc9a70ba28eebb012c6c359602e3ef76ba2
Ahmed Orabi <aorabi@outlook.com>
2022-03-21 22:36:39 +0300

    initial

---
+++ server.js
@@ -0,0 +1,43 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+const dataArray = [];
+
+// Require Express to run server and routes
+const express = require("express");
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
+
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 8080;
+
+
+
+  //get
+  app.get("/all", function (req, res) {
+      console.log(dataArray);
+    res.status(200).send(dataArray);
+    console.log(data);
+  });
+
+
+  const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`Hi all - server is running on port ${port}`);
+  }
---
+++ website/app.js
@@ -0,0 +1,7 @@
+/* Global Variables */
+const myApiKey = '82ac8ab60b058edcb24b0750c0d721b2'
+
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();

f50e912ba8b056b2a4ae2371b8c3deb135f769af
Ahmed Orabi <aorabi@outlook.com>
2022-03-22 20:46:38 +0300

    post data to server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {date: "" , temp : "" , feelins : ""};
 const dataArray = [];
 
 
@@ -23,16 +23,36 @@ const cors = require("cors");
 app.use(cors());
 
 
-//get
-app.get("/all", function (req, res) {
-  
-res.status(200).send(dataArray);
-console.log(dataArray);
-});
+
+
 
 // Initialize the main project folder
  app.use(express.static('website'));
 
+
+ //get
+app.get("/getTemp", function (req, res) {
+  
+projectData = dataArray[0];
+
+  res.status(200).send(projectData);
+  console.log(projectData);
+  });
+  
+
+   //get
+app.post("/setTemp", function (req, res) {
+  
+  let data1 = req.body;
+  console.log(data1);
+
+
+//console.log(req.body)
+
+dataArray.push(req.body)
+res.status(200).send(req.body)
+
+  });
 
 // Setup Server
 const port = 8080;
--- website/app.js
+++ website/app.js
@@ -1,15 +1,18 @@
+//const res = require("express/lib/response");
+
 /* Global Variables */
 const ApiKey = '82ac8ab60b058edcb24b0750c0d721b2'
 const basicUrl = 'https://api.openweathermap.org/data/2.5/weather?'
 const ApiUnit = 'metric'
-
+let city = ''
 
 //get html Elements by ID
 const btnGenerate = document.getElementById('btnGenerate');
 
-const txtFeelings = document.getElementById('feelings')
+
 const txtDate = document.getElementById('date')
 const txtTemp = document.getElementById('temp')
+
 const txtContent = document.getElementById('content')
 
 
@@ -17,6 +20,7 @@ const txtContent = document.getElementById('content')
 
 btnGenerate.addEventListener('click' , async () => {
 const ZipCode = document.getElementById('zip').value
+const txtfeelings = document.getElementById("feelings").value
 
 const weatherUrl = `${basicUrl}zip=${ZipCode},us&appid=${ApiKey}&units=${ApiUnit}`
 
@@ -24,27 +28,66 @@ const Data = await  fetch(weatherUrl)
 
 if (Data.status === 200){
 
-    const wData = await Data.json()
+    const jdata = await Data.json()
 
-    temp = `${wData["main"]["temp"]} °C `
+    temp = `${jdata["main"]["temp"]} °C `
     
-    const city = wData.name 
+     city = jdata.name 
+
+    
     
     console.log( city + "=" + temp)
+    txtContent.style.backgroundColor="white"
 
 }else {
 
-    console.log( 'not found')
+    console.log( 'Not found')
+    txtContent.innerHTML = `Not found`
+    txtContent.style.backgroundColor="red"
 
 }
 
-
-
+const response = await postWeatherData("/setTemp", { date: newDate , temp: temp , feelins: txtfeelings });
+txtContent.innerHTML = `City :- ${city}   <hr>
+                       Temp :- ${response.temp} <hr>
+                        date :- ${response.date} <hr>
+                       feeling :- ${response.feelins}`
 
 })
 
 
+const postWeatherData = async (url = "", data = {}) => {
+    // console.log(data);
+   
+    
+     const response = await fetch(url, {
+       method: "POST",
+       credentials: "same-origin",
+       headers: {
+         "Content-Type": "application/json",
+       },
+       // Body data type must match "Content-Type" header
+       body: JSON.stringify(data),
+     });
+     try {
+       const newData = await response.json();
+       //console.log(newData);
+       console.log(data)
+       return newData;
+     } catch (error) {
+       console.log("error", error);
+     }
+   };
+
+
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
 
+
+
+
+
+
+//postWeatherData("/setTemp", { date: newDate , temp: txtTemp , f: txtFeelings });
